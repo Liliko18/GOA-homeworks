0b2160de2f19ad57316f6299d2b383fc7b2cbe53
Liliko18 <lili.tomaradze@gmail.com>
2025-11-22 06:56:23 -0700

    1

---
+++ level17/hw/hm.n9.py
@@ -0,0 +1,28 @@
+num1 = int(input("enter any number:"))
+num2 = int(input("enter any number:"))
+operator = input("enter + - * / ** < > == !=" ) 
+
+if operator == "+":
+    print (num1 + num2)
+elif operator == "-":
+    print (num1 - num2)
+elif operator == "*":
+     print (num1 * num2)
+elif oprrator == "/":
+    print (num1 / num2)
+elif operator == "**":
+    print (num1 ** num2)
+elif operator == ">":
+    print (num1 > num2)
+elif operator == "<":
+    print (num1 < num2)
+elif operator == "==":
+    print (num1 == num2)
+elif oprator == "!=":
+    print (num1 != num2)
+elif operator == "%":
+    print (num1 % num2)
+
+
+
+
